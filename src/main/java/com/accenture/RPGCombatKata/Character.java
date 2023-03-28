9bb5160fba6a7d4840e6596e89dd4baf35fc669a
jaume vidal <jaumevibu@gmail.com>
2023-03-28 15:10:11 +0200

    Pass characterStartsWithCorrectsProps to CharacterTest

--- src/main/java/com/accenture/RPGCombatKata/Character.java
+++ src/main/java/com/accenture/RPGCombatKata/Character.java
@@ -2,4 +2,20 @@ package com.accenture.RPGCombatKata;
 
 public class Character {
 
+    private Long health=1000L;
+    private Long level=1L;
+    private Boolean alive=true;
+
+    public Long getHealth() {
+        return this.health;
+    }
+
+    public Long getLevel() {
+        return this.level;
+    }
+
+    public Boolean isAlive(){
+        return this.alive;
+    }
+
 }
